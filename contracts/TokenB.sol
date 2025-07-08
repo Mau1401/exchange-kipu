421cd981b056d397d3d0ae9a6e63bf30d92cc3b0
Mau1401 <mauricio.rodriguezobando@ucr.ac.cr>
2025-07-07 23:47:33 -0600

    change import on tokens

--- contracts/TokenB.sol
+++ contracts/TokenB.sol
@@ -2,7 +2,7 @@
 pragma solidity ^0.8.20;
 
 //import "./SimpleERC20TokenOZ.sol"; 
-import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/v4.9.0/contracts/token/ERC20/ERC20.sol";
 
 contract TokenB is ERC20 {
     constructor(uint256 initialSupply) ERC20("TokenB", "TKNB") {
